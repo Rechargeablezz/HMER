da8cf18cc501d0083d9120fb7aca4612bdc85aea
Rechargeablezz <shenhaoyang6@gmail.com>
2024-01-03 14:51:13 +0800

    [feat]

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# JSUB -q gpu
+# JSUB -gpgpu 4
+# JSUB -n 4
+# JSUB -e jsub_logs/error.%J
+# JSUB -o jsub_logs/output.%J
+source /apps/software/anaconda3/etc/profile.d/conda.sh
+
+conda activate can
+unset PYTHONPATH
+
+python train.py --dataset CROHME2
